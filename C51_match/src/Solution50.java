0c8c00bf4ab1d7b529cf9010d42fc305720adb2c
weihansheng <johan007@126.com>
2018-08-11 22:42:50 +0800

    C51 正则表达式

--- C51_match/src/Solution50.java
+++ C51_match/src/Solution50.java
@@ -1,52 +1,82 @@
 import java.util.*;
 
 public class Solution50 {
+    /**
+     * 请实现一个函数用来匹配包括'.'和'*'的正则表达式。模式中的字符
+     * '.'表示任意一个字符， 而'*'表示它前面的字符可以出现任意次（包含0次）。
+     * 在本题中，匹配是指字符串的所有字符匹配整个模式。
+     * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配
+     *
+     *
+     解这题需要把题意仔细研究清楚，反正我试了好多次才明白的。
+         首先，考虑特殊情况：
+              1>两个字符串都为空，返回true
+              2>当第一个字符串不空，而第二个字符串空了，返回false（因为这样，就无法
+                 匹配成功了,而如果第一个字符串空了，第二个字符串非空，还是可能匹配成
+                 功的，比如第二个字符串是“a*a*a*a*”,由于‘*’之前的元素可以出现0次，
+                 所以有可能匹配成功）
+         之后就开始匹配第一个字符，这里有两种可能：匹配成功或匹配失败。但考虑到pattern
+         下一个字符可能是‘*’， 这里我们分两种情况讨论：pattern下一个字符为‘*’或
+         不为‘*’：
+               1>pattern下一个字符不为‘*’：这种情况比较简单，直接匹配当前字符。如果
+                 匹配成功，继续匹配下一个；如果匹配失败，直接返回false。注意这里的
+                 “匹配成功”，除了两个字符相同的情况外，还有一种情况，就是pattern的
+                 当前字符为‘.’,同时str的当前字符不为‘\0’。
+               2>pattern下一个字符为‘*’时，稍微复杂一些，因为‘*’可以代表0个或多个。
+                 这里把这些情况都考虑到：
+                    a>当‘*’匹配0个字符时，str当前字符不变，pattern当前字符后移两位，
+                     跳过这个‘*’符号；
+                    b>当‘*’匹配1个或多个时，str当前字符移向下一个，pattern当前字符
+                     不变。（这里匹配1个或多个可以看成一种情况，因为：当匹配一个时，
+                     由于str移到了下一个字符，而pattern字符不变，就回到了上边的情况a；
+                     当匹配多于一个字符时，相当于从str的下一个字符继续开始匹配）
+         之后再写代码就很简单了。
+     * @param str
+     * @param pattern
+     * @return
+     */
     //"",".*" 为true "","c*"
     public boolean match(char[] str, char[] pattern) {
-        if (str == null || pattern == null) {
-            return false;
-        }
-        if (str.length == 0 && pattern.length == 0) {
+        if (str.length==0&&pattern.length==0)
             return true;
-        }
-        int i = 0, j = 0;
-        while (i < str.length && j < pattern.length) {
-            if (str[i] == pattern[j] || pattern[j] == '.') {
-                i++;
-                j++;
-            } else if (pattern[j] == '*') {
-                if (j > 0 & str[i] == pattern[j - 1]) {
-                    i++;
-                } else {
-                    j++;
-                }
-            }
-//            else if(j>0&i>0&pattern[j-1]=='*'&pattern[j]==str[i-1]){
-//                j++;
-//            }
-            else {
-                j++;
-            }
-
-        }
-        if (pattern.length - j == 2 & pattern[pattern.length - 1] == '*'){
-            return true;
-        }
-        if (i==str.length&&j==pattern.length){
-            return true;
-        }else {
-            return false;
-        }
-
+        return matchCore(str,0, pattern,0);
     }
+
+    boolean matchCore(char[] str, int i, char[] pattern,int j) {
+        //有效性检验：str到尾，pattern到尾，匹配成功
+        if (str.length>0&&pattern.length==0)
+            return true;
+        //pattern先到尾，匹配失败
+        if (i != str.length && j == pattern.length)
+            return false;
+        //模式第2个是*，且字符串第1个跟模式第1个匹配,分3种匹配模式；如不匹配，模式后移2位
+        if (j + 1 < pattern.length && pattern[j + 1] == '*') {
+            if ((i != str.length && pattern[j] == str[i]) || (pattern[j] == '.' && i != str.length)) {
+                return matchCore(str, i, pattern, j + 2)//模式后移2，视为x*匹配0个字符
+                        || matchCore(str, i + 1, pattern, j + 2)//视为模式匹配1个字符
+                        || matchCore(str, i + 1, pattern, j);//*匹配1个，再匹配str中的下一个
+            } else {
+                return matchCore(str, i, pattern, j + 2);
+            }
+        }
+        //模式第2个不是*，且字符串第1个跟模式第1个匹配，则都后移1位，否则直接返回false
+        if ((i != str.length && pattern[j] == str[i]) || (pattern[j] == '.' && i != str.length)) {
+            return matchCore(str, i + 1, pattern, j + 1);
+        }
+        return false;
+    }
+
 
     public static void main(String[] args) {
         Solution50 solution50=new Solution50();
-//        String str="";
-        String str="aaa";
+        String str="";
+//        String str="aaa";
+//        String pattern="";
+        String pattern=".";
+//        String pattern=".*";
 //        String pattern="ab*ac*a";
 //        String pattern="ab*a";
-        String pattern="a.a";
+//        String pattern="a.a";
 //        String pattern="aa.a";
 //        String pattern="a*";
         System.out.println(solution50.match(str.toCharArray(),pattern.toCharArray()));
